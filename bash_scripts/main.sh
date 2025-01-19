e9f17334a76e625afd8cd01233a48da9ac5573a3
gietema <jochem@giete.ma>
2025-01-19 17:18:03 +0000

    Simplify bash

--- bash_scripts/main.sh
+++ bash_scripts/main.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-declare -A tasks=(
-  ["translation"]="en_es en_fr es_en fr_en"
-  ["linguistic"]="antonyms singular_plural present_simple_gerund present_simple_past_simple"
-  ["knowledge"]="country_capital location_continent location_religion person_language"
-)
+# Define categories and tasks as separate arrays
+categories=("translation" "linguistic" "knowledge")
+translation_tasks=("en_es" "en_fr" "es_en" "fr_en")
+linguistic_tasks=("antonyms" "singular_plural" "present_simple_gerund" "present_simple_past_simple")
+knowledge_tasks=("country_capital" "location_continent" "location_religion" "person_language")
 
 demonstrations=(2 5 10 20)
 
-for category in "${!tasks[@]}"; do
-  for task in ${tasks[$category]}; do
+# Loop through each category
+for category in "${categories[@]}"; do
+  # Dynamically get the tasks array for the current category
+  tasks_var="${category}_tasks[@]"
+  tasks=("${!tasks_var}")
+
+  for task in "${tasks[@]}"; do
     for nb_demonstrations in "${demonstrations[@]}"; do
-      poetry run python scripts/main.py --model-size 1B --task "$category/$task" --nb-runs 5 --nb-demonstrations "$nb_demonstrations"
+      poetry run python scripts/main.py --model-size 1B --task "$category/$task" --nb-runs 2 --nb-demonstrations "$nb_demonstrations"
     done
   done
 done
